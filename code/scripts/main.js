9e754b7f200f151d2f08e3895ca5dd4d757816a1
かろん <104133597+karonyt@users.noreply.github.com>
2023-02-12 02:46:40 +0900

    Update and rename update to code/scripts/main.js

---
+++ code/scripts/main.js
@@ -0,0 +1,34 @@
+import { world, ItemStack } from "@minecraft/server";
+
+const prefix = "?l_"
+world.events.beforeChat.subscribe(chat => {
+    const { sender: player, message } = chat;
+    if (!message.startsWith(prefix)) return;
+    if (!player.hasTag("lore")) { 
+      player.tell("§cloreTagが付いてないためこのコマンドは使えません")
+      chat.cancel = true;
+      return;
+    }
+  
+    chat.cancel = true;
+    const command = message.slice(prefix.length).split(" ")[0];
+    const args = message.slice(prefix.length + command.length + 1).split(/\n/);
+
+    switch (command) {
+        case "set": {
+            const item = player.getComponent("inventory").container.getItem(player.selectedSlot);
+            if (!item) { 
+              player.tell("§cアイテムを持っていません")
+              return;
+            }
+            item.setLore(args);
+            player.getComponent("inventory").container.setItem(player.selectedSlot,item);
+            player.tell(`§a手持ちのアイテムの説明文を ${args} §r§aにしました`)
+            break;
+        }
+        default: {
+            player.tell("そのようなコマンドはありません。");
+            break;
+        }
+    }
+});
