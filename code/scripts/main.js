c89f9316d3452cf5ddd5a2f2900b334b28fc01c8
かろん <104133597+karonyt@users.noreply.github.com>
2023-06-08 21:51:38 +0900

    Update main.js

--- code/scripts/main.js
+++ code/scripts/main.js
@@ -1,34 +1,34 @@
-import { world, ItemStack } from "@minecraft/server";
+import { world , system} from "@minecraft/server";
 
 const prefix = "?l_"
-world.events.beforeChat.subscribe(chat => {
-    const { sender: player, message } = chat;
-    if (!message.startsWith(prefix)) return;
-    if (!player.hasTag("lore")) { 
-      player.sendMessage("§cloreTagが付いてないためこのコマンドは使えません")
-      chat.cancel = true;
-      return;
-    }
-  
+world.beforeEvents.chatSend.subscribe(chat => {
+  const { sender: player, message } = chat;
+  if (!message.startsWith(prefix)) return;
+  if (!player.hasTag("lore")) { 
+    player.sendMessage("§cloreTagが付いてないためこのコマンドは使えません")
     chat.cancel = true;
-    const command = message.slice(prefix.length).split(" ")[0];
-    const args = message.slice(prefix.length + command.length + 1).split(/\n/);
-
-    switch (command) {
-        case "set": {
-            const item = player.getComponent("inventory").container.getItem(player.selectedSlot);
-            if (!item) { 
-              player.sendMessage("§cアイテムを持っていません")
-              return;
-            }
-            item.setLore(args);
-            player.getComponent("inventory").container.setItem(player.selectedSlot,item);
-            player.sendMessage(`§a手持ちのアイテムの説明文を ${args} §r§aにしました`)
-            break;
-        }
-        default: {
-            player.sendMessage("そのようなコマンドはありません。");
-            break;
-        }
+    return;
+  }
+  chat.cancel = true;
+  const command = message.slice(prefix.length).split(" ")[0];
+  const args = message.slice(prefix.length + command.length + 1).split(/\\n/);
+  switch (command) {
+    case "set": {
+      const item = player.getComponent("inventory").container.getItem(player.selectedSlot);
+      if (!item) { 
+        player.sendMessage("§cアイテムを持っていません")
+        return;
+      }
+      system.runTimeout(() => {
+        item.setLore(args);
+        player.getComponent("inventory").container.setItem(player.selectedSlot,item);
+        player.sendMessage(`§a手持ちのアイテムの説明文を ${args} §r§aにしました`)
+      })
+      break;
     }
+    default: {
+      player.sendMessage("そのようなコマンドはありません。");
+      break;
+    }
+  }
 });
